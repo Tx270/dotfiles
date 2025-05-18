56c5dda640fed9ba6058c530564e3cc4d77eb1f0
Tx270 <150079627+Tx270@users.noreply.github.com>
2025-05-18 23:23:54 +0200

    update installUser.sh 4

--- installUser.sh
+++ installUser.sh
@@ -11,7 +11,7 @@ pause() {
 }
 
 log_section "Setting up Fonts"
-if [ ! -f "$HOME/.local/share/fonts/Meslo/MesloLGS NF Regular.ttf" ]; then
+if [ ! -d "$HOME/.local/share/fonts/Meslo" ]; then
   mkdir -p "$HOME/.local/share/fonts/" && cd /tmp || exit 1
   wget -q https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip -O Meslo.zip || exit 2
   unzip -oq Meslo.zip -d Meslo || exit 3
@@ -35,6 +35,9 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]
 fi
 if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom/}/plugins/zsh-allclear" ]; then
+    git clone https://github.com/givensuman/zsh-allclear ${ZSH_CUSTOM:-~/.oh-my-zsh/custom/}/plugins/zsh-allclear
 fi
 pause
 
@@ -61,15 +64,16 @@ log_section "Installing Ruby Tools"
 pause
 
 log_section "Applying Dotfiles"
-rm -r ~/.config/neofetch
+rm -rf ~/.config/neofetch ~/.zshrc ~/.config/i3
 cd ~/.dotfiles
 stow */
 find ~/.config -name "*.sh" -type f -exec chmod +x {} \;
 pause
 
 log_section "Setting up Sysmonitor script"
-if ! crontab -l | grep -q sysmonitor.sh; then
-    (crontab -l 2>/dev/null; echo "*/10 * * * * ~/.config/sysmonitor.sh") | crontab -
+if ! sudo crontab -l 2>/dev/null | grep -q sysmonitor.sh; then
+    (sudo crontab -l 2>/dev/null; echo "*/10 * * * * $HOME/.config/sysmonitor.sh") | sudo crontab -
 fi
+pause
 
 echo "User environment ready. You can reboot now."
